083d3aefadf3dd8256463811feaa8438d5834ff3
boolaz <bv@ixus.net>
2016-08-25 13:20:04 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# samsungSMS
+
+If you try to extract the content from some Samsung mobile phones (B550H and some other types as well) with Cellebrite UFED, you won't be able to dump the list of the SMS sent and received from the device. If you try to do it with MSAB XRY, you'll be presented with a bunch of .VMG files that might be very numerous (one for each SMS). Therefore, presenting them in your report might be a tough task to achieve.
+
+The purpose of this tool is to convert all the .VMG files resulting from an XRY extraction to a neat CSV file that you can use to build your report in a simple way.
+
+Of course, this script I release today is related to Samsung phones and can be adapted to various types of mobile phones.
+
+## Prerequisites
+
+samsungSMS has been developed in python 2.7 and has been successfully tested on Linux Ubuntu 14.04 LTS, MacOSX 10.11.6 El Capitan and Windows 8.1 x64. This tool doesn't require any additional python modules to be run.
+A stand-alone binary version for windows is also available for download.
+
+## Syntax
+
+Simply execute the python script or the windows binary with the directory containing the .VMG SMS files as an arguments
+
+    $ ./samsungSMS.py SMS_directory
+
+As an output, you'll get a text CSV file with the details of the SMS files
+
+
+Stay tuned for updates and please, feel free to report any bug to the author.
---
+++ SamsungSms.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#  ------------------------------------------------------------------------
+#  NOM : samsungSMS.py
+#  AUTEUR : Bruno VALENTIN (bruno@boolaz.com)
+#  VERSION : 1.0
+#  DATE : 27/07/2016
+#  DESCRIPTION : parses SMS from Samsung SMB-550H Cellphone (.vmg files)
+
+import os,sys,re
+from sys import argv
+import fnmatch
+
+softdesc={"name":"samsungSMS",
+          "version":"1.0", \
+          "release":"27/07/2016", \
+          "purpose":"parses SMS from Samsung SMB-550H Cellphone", \
+          "link":"https://github.com/boolaz/samsungSMS" }
+
+usage_text="""Usage: samsungSMS.py SMS_directory"""
+
+extensions=["*.vmg"]
+
+#-------------------------
+class Banner(object):
+    """Banner for the program"""
+    def __init__(self, banner_values):
+        super(Banner, self).__init__()
+        self.banner_values = banner_values
+
+    def display(self):
+        print("""
+****************************************************** \n\
+* {0} {1} ({2}) - bruno@boolaz.com \n\
+* {3}           \n\
+* Updates : {4} \n\
+******************************************************""" \
+         .format(self.banner_values['name'],self.banner_values['version'],
+                 self.banner_values['release'],self.banner_values['purpose'],
+                 self.banner_values['link']))
+
+#-------------------------
+def usage(usage_text):
+    """Usage"""
+    print usage_text
+
+#-------------------------
+def replace_special_chars(sms_text):
+	sms_text=sms_text.replace("=C5=93","oe")
+	sms_text=sms_text.replace("=C3=A9","é")
+	sms_text=sms_text.replace("=C3=A8","è")
+	sms_text=sms_text.replace("=C3=A7","ç")
+	sms_text=sms_text.replace("=C3=AA","ê")
+	sms_text=sms_text.replace("=C3=A0","à")
+	sms_text=sms_text.replace("=C3=A2","â")
+	sms_text=sms_text.replace("=C3=87","C")
+	sms_text=sms_text.replace("=C3=80","A")
+	sms_text=sms_text.replace("=C3=87","C")
+	sms_text=sms_text.replace("=C3=AE","î")
+	sms_text=sms_text.replace("=C3=AF","ï")
+	sms_text=sms_text.replace("=C3=B4","ô")
+	sms_text=sms_text.replace("=C3=AC","i")
+	sms_text=sms_text.replace("=C3=B6","ô")
+	return sms_text
+
+#-------------------------
+def main(argv):
+
+	my_banner=Banner(softdesc)
+	my_banner.display()
+
+	# parses arguments ----
+	try:
+		chemin=sys.argv[1]
+	except:
+		usage(usage_text)
+		sys.exit(2)
+
+	# search for files ----
+	fichiers=[]
+	try:
+		for root, dirs, files in os.walk(chemin, topdown=False):
+			for extension in extensions:
+				for name in fnmatch.filter(files, extension):
+					fichiers.append(os.path.join(root, name))
+	except:
+		print 'A problem occured during file enumeration'
+
+	# analysis of files
+	out_file=open("SMS.csv","w")
+	for fichier in fichiers:
+		sms_status=sms_box=sms_cell=sms_date=sms_text=''
+		in_body=False
+		in_file=open(fichier,"r")
+		for line in in_file:
+			match_flag=False
+			if not match_flag:
+				value=re.search("X-IRMC-STATUS:([a-z]+)", "{0}".format(line), re.I)
+				if value:
+					sms_status=value.group(1)
+					match_flag=True
+
+			if not match_flag:
+				value=re.search("X-IRMC-BOX:([a-z]+)", "{0}".format(line), re.I)
+				if value:
+					sms_box=value.group(1)
+					match_flag=True
+
+			if not match_flag:
+				value=re.search("TEL;CELL:([0-9+]+)", "{0}".format(line), re.I)
+				if value:
+					sms_cell=value.group(1)
+					match_flag=True
+
+			if not match_flag:
+				value=re.search("Date:(.+)\n$", "{0}".format(line), re.I)
+				if value:
+					sms_date=value.group(1)
+					sms_date=sms_date.strip('\r')
+					match_flag=True
+
+			if not match_flag:
+				value=re.search("TEXT;[^:]+:(.+)$", "{0}".format(line), re.I)
+				if value:
+					sms_text=value.group(1).strip('\r\n')
+					sms_text = re.sub(r"=$", "", sms_text)
+					#sms_text=sms_text
+					in_body=True
+					match_flag=True
+
+			if not match_flag and in_body:
+				value=re.search("END:VBODY", "{0}".format(line), re.I)
+				if value:
+					in_body=False
+				else:
+					sms_text+=re.sub(r"=$", "", line.strip('\r\n'))
+
+		if sms_date:
+			sms_text=sms_text.replace("\n",". ")
+			try:
+				out_file.write ("{0}\t{1}\t{2}\t{3}\t{4}\n".format(sms_date,sms_cell,sms_box,sms_status,replace_special_chars(sms_text)))
+			except:
+				print "Error occured exporting file"
+
+		in_file.close()
+
+	out_file.close()
+	print "{0!s} SMS exported to SMS.csv".format(len(fichiers))
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
